8bac228e2f99e16e4b6fd5f558c52f5b38daeadc
Angel Ricardo <angelricardo.dev@gmail.com>
2020-11-07 00:10:19 -0600

    increased limit on json requests

--- app/src/server/app.ts
+++ app/src/server/app.ts
@@ -7,7 +7,7 @@ export const application = express()
 // SERVER SETUP
 application.set('app_name', process.env.APP_NAME)
 application.set('port', process.env.PORT || process.env.DEV_PORT)
-application.use(express.json())
+application.use(express.json({ limit: '50mb' }))
 
 // SERVER ROUTES
 application.use('/api', api_routes)
