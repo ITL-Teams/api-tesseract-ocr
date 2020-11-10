123b0a56241983bf844e3d210954f911d55112a2
Angel Ricardo <angelricardo.dev@gmail.com>
2020-11-10 17:18:15 -0600

    Added ImageParser
    Text parser component, it takes text output from OCR and transforms that into a students list

---
+++ app/src/services/ImageParser.ts
@@ -0,0 +1,66 @@
+import { IService } from './IService'
+
+export type ParserOutput = {
+  names: string[]
+}
+
+enum PARSER_STATES {
+  IN_NAME_READING,
+  IGNORE,
+  NEXT_NAME
+}
+
+export class ImageParserService implements IService<ParserOutput> {
+  private data: string
+
+  constructor(data: string) {
+    this.data = data
+  }
+
+  public invoke(): ParserOutput {    
+    const dataTokens = this.data.split(/\s+/)
+    const names = this.parseNames(dataTokens)
+
+    return {
+      names: names
+    }
+  }
+
+  private parseNames(dataTokens: string[]): string[] {
+    let names: string[] = []
+    let parserState: PARSER_STATES = PARSER_STATES.IN_NAME_READING
+
+    let actual_name:string = ''
+    dataTokens.forEach(token => {
+        parserState = this.parserState(token, parserState)
+
+        if(parserState === PARSER_STATES.IGNORE)
+          return
+        if(parserState === PARSER_STATES.NEXT_NAME) {
+          names.push(actual_name)
+          actual_name = ''
+          return
+        }
+        
+        actual_name = `${actual_name} ${token}`
+    });
+
+    return names
+  }
+
+  private parserState(token: string, actual_state: PARSER_STATES): PARSER_STATES {
+    if(token === '7')
+      return PARSER_STATES.IGNORE
+    
+    if(token === 'Presente')
+      return PARSER_STATES.NEXT_NAME
+
+    if(actual_state === PARSER_STATES.NEXT_NAME)
+      return PARSER_STATES.IN_NAME_READING
+
+    if(actual_state === PARSER_STATES.IN_NAME_READING)
+      return actual_state
+
+    return PARSER_STATES.IGNORE
+  }
+}
